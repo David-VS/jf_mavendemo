397980cc66c9eb9fb3983f44d2496143c75c6a43
david.van.steertegem <david.van.steertegem@ehb.be>
2023-04-25 10:53:54 +0200

    Initial commit

---
+++ src/main/java/be/ehb/mavendemo/Main.java
@@ -0,0 +1,28 @@
+package be.ehb.mavendemo;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String url = "https://api.chucknorris.io/jokes/random";
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url(url)
+                .get()
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            System.out.println(response.body().string());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
