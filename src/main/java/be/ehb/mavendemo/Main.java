c7996b08a51580758cbffbd2f3b48788b5ee2ebb
david.van.steertegem <david.van.steertegem@ehb.be>
2023-04-25 12:44:38 +0200

    added dependencies, quick test in main

--- src/main/java/be/ehb/mavendemo/Main.java
+++ src/main/java/be/ehb/mavendemo/Main.java
@@ -1,14 +1,19 @@
 package be.ehb.mavendemo;
 
+
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+import org.apache.commons.lang3.tuple.Pair;
 
 import java.io.IOException;
 
 public class Main {
 
     public static void main(String[] args) {
+
+        Pair<String, String> pair = Pair.of("Hello", "World");
+        System.out.println(pair);
 
         String url = "https://api.chucknorris.io/jokes/random";
         OkHttpClient client = new OkHttpClient();
